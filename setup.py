08b08c63059dc23efb7225f24f38b6e50efb63c1
Vioshim <Krangelj@unicartagena.edu.co>
2024-05-01 19:13:45 -0500

    Refactor setup.py to update URL and add additional metadata

--- setup.py
+++ setup.py
@@ -1,12 +1,43 @@
+from os import path
+
 from setuptools import setup
+
+DESCRIPTION_PATH = path.join(path.abspath(path.dirname(__file__)), "README.md")
+with open(DESCRIPTION_PATH, encoding="utf-8") as f:
+    long_description = f.read()
+
+REQUIREMENTS_PATH = path.join(path.abspath(path.dirname(__file__)), "requirements.txt")
+with open(REQUIREMENTS_PATH, encoding="utf-8") as f:
+    requirements = f.read()
+
 
 setup(
     name="PySynth",
-    version="2.4.1",
-    description="A simple music synthesizer for Python 3",
     author="Martin C. Doege",
-    author_email="mdoege@compuserve.com",
-    url="http://mdoege.github.io/PySynth/",
+    url="https://github.com/Vioshim/PySynth",
+    keywords=["music", "piano", "notes"],
+    version="2.4.1",
     packages=["pysynth"],
-    requires=["numpy"],
+    include_package_data=True,
+    license="GNU General Public License v3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description="A simple music synthesizer for Python 3",
+    python_requires=">=3.10.0",
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Intended Audience :: Musicians",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Internet",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Multimedia :: Sound/Audio :: MIDI",
+        "Topic :: Multimedia :: Sound/Audio :: Sound Synthesis",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
