dd0379fb84413bb2161b6fb0da3cb35bb8e4d9fa
Vioshim <Krangelj@unicartagena.edu.co>
2024-05-01 19:04:26 -0500

    Update version number in setup.py to 2.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="PySynth",
-    version="2.4",
+    version="2.4.1",
     description="A simple music synthesizer for Python 3",
     author="Martin C. Doege",
     author_email="mdoege@compuserve.com",
